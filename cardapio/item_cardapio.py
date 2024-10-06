c0c36d43794eaa660bad36caf43d6d99cba2eb4e
MaiconJordan <maiconplus@gmail.com>
2024-10-06 13:16:47 -0300

    classe abstrada

--- cardapio/item_cardapio.py
+++ cardapio/item_cardapio.py
@@ -1,4 +1,12 @@
-class ItemCardapio:
+from abc import ABC, classmethod
+
+
+class ItemCardapio(ABC): 
+    
     def __init__(self, nome, preco):
         self._nome = nome
         self._preco = preco
+
+@classmethod
+def aplicar_desconto(self):
+    pass
