55da344dc7e6bfd3b51d7e2d01acb01ed206cb11
MaiconJordan <maiconplus@gmail.com>
2024-10-05 21:44:34 -0300

    funcao add bebia e prato

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from Restaurante import Restaurante
+from cardapio.bebida import Bebida
+from cardapio.prato import Prato
 
-restaurante_praca = Restaurante('Nome', 'teste')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Gui', 5)
-restaurante_praca.receber_avaliacao('Gui', 5)
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida ('Suco de Melancia', 5.0, 'grande')
+prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
+
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
