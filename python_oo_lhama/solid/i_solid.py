ab4d65211d04082df2c1b3fbb2fc4c781b15b38c
Maicon Jordan <maiconplus@gmail.com>
2025-07-25 20:16:18 -0300

    i de solid exemplo correto

--- python_oo_lhama/solid/i_solid.py
+++ python_oo_lhama/solid/i_solid.py
@@ -29,3 +29,26 @@ class Robô(Trabalhador):
         raise NotImplementedError("Robôs não comem")
 
 
+#EXEMPLO CORRETO QUE RESPEITA O I DE SOLID
+
+class Trabalhador(ABC):
+    @abstractmethod
+    def trabalhar(self):
+        pass
+
+class Comedor(ABC):
+    @abstractmethod
+    def comer(self):
+        pass
+
+
+class Humano(Trabalhador, Comedor):
+    def trabalhar(self):
+        print("Humano está trabalhando")
+
+    def comer(self):
+        print("Humano está comendo")
+
+class Robo(Trabalhador):
+    def trabalhar(self):
+        print("Robô está trabalhando")
