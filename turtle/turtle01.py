52950b09138d89624cb5e95f4caa7c97ab95cafa
Maicon Jordan <maiconplus@gmail.com>
2025-07-26 21:51:06 -0300

    turtle circulo

---
+++ turtle/turtle01.py
@@ -0,0 +1,12 @@
+import turtle
+
+
+# Cria a janela e a tartaruga
+tela = turtle.Screen()
+t = turtle.Turtle()
+
+# Desenha um círculo de raio 100
+t.circle(100)
+
+# Mantém a janela aberta até que seja fechada
+tela.mainloop()
