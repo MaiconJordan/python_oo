302b06d358d7f2e6068f8798649a6007e5636ec4
Maicon <maiconplus@gmail.com>
2025-07-30 21:52:25 -0300

    metodo validate_fields

--- python_mvc_lhama/src/controller/people_finder_controller.py
+++ python_mvc_lhama/src/controller/people_finder_controller.py
@@ -9,3 +9,9 @@ class PeopleFinderController:
             return {"success": True, "message": response}
         except Exception as exception:
             return {"success": False, "message": str(exception)}
+        
+    def __validate_fields(self, person_finder_information: Dict) -> None:
+        if not isinstance(person_finder_information["name"], str):
+            raise ValueError("O nome é obrigatório e deve ser uma string não vazia.")
+        
+    
